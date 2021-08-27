82c3ce771acc34c5af4dfb1a10b5e8e3607c3b1e
katayama1125 <76419146+katayama1125@users.noreply.github.com>
2021-08-27 15:11:48 +0900

    Initial commit

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+
+    public GameObject GoalText;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Flower")
+        {
+            this.GoalText.SetActive(true);
+        }
+    }
+}
